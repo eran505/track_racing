c691186f57c1d2b41f83c8e22da6091f0a6f0f32
ERANHER <eran505@gmail.com>
2020-01-29 15:55:12 +0200

    g

---
+++ cmake-build-debug/CMakeFiles/track_racing.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/track_racing.dir/Agent.cpp.o"
+  "CMakeFiles/track_racing.dir/Game.cpp.o"
+  "CMakeFiles/track_racing.dir/Grid.cpp.o"
+  "CMakeFiles/track_racing.dir/MdpPlaner.cpp.o"
+  "CMakeFiles/track_racing.dir/State.cpp.o"
+  "CMakeFiles/track_racing.dir/headers/Policy/RTDP_util.cpp.o"
+  "CMakeFiles/track_racing.dir/headers/Policy/RtdpAlgo.cpp.o"
+  "CMakeFiles/track_racing.dir/headers/serach/Astar.cpp.o"
+  "CMakeFiles/track_racing.dir/main.cpp.o"
+  "CMakeFiles/track_racing.dir/util_game.cpp.o"
+  "track_racing"
+  "track_racing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/track_racing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
