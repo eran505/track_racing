e59fb71e3cadea00c7b8d5616b941926299f7dcf
eran <eran505@gmail.com>
2020-04-06 02:18:48 +0300

    04/06 02:18:48

--- headers/Policy/PathPolicy.hpp
+++ headers/Policy/PathPolicy.hpp
@@ -50,7 +50,7 @@ public:
                     auto endP = std::get<0>(goalPoint[k]);
                     weightEnd = std::get<1>(goalPoint[k]);
                     auto zeroSrc  = Point();
-                    auto startSpeed  = Point(0,0,max_speed);
+                    auto startSpeed  = Point(0,0,0);
                     //auto zeroDest  = Point();
                     auto src = AStar::StatePoint{Point(*startP),startSpeed};
                     auto dest = AStar::StatePoint{Point(*endP),startSpeed};
