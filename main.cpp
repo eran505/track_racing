76f928cbb545018359893de7e1b5c8d2cd801d24
eran <eran505@gmail.com>
2020-05-05 18:29:15 +0300

    05/05 18:29:15

--- main.cpp
+++ main.cpp
@@ -53,11 +53,7 @@ typedef vector<tuple<Point*,double>> listPointWeighted;
 typedef unsigned long ulong;
 int main() {
 
-//    std::tuple<int, std::string> t(42, "foo");
-//    const auto& [i, s] = t;
-//    cout<<i<<endl;
-//    cout<<s<<endl;
-//    exit(0);
+
     int seed = 155139;
     seed = 1587982523; //1895975606
     //seed = int( time(nullptr));
@@ -70,7 +66,7 @@ int main() {
     string repo = "/"+arrPAth[0]+"/"+arrPAth[1]+"/"+arrPAth[2]+"/"+arrPAth[3]+"/"+arrPAth[4];
     int MaxInt = INT_MAX;
     //const string home="/home/ise";
-    std::string pathCsv (home + "/car_model/config/con_test.csv");
+    std::string pathCsv (home + "/car_model/config/con3.csv");
     std::string toCsvPath (home+ "/car_model/exp/out/");
     auto csvRows = readConfigFile(pathCsv);
     int ctrId=1;
@@ -127,7 +123,7 @@ Game* initGame(configGame &conf ){
     //exit(0);
     cout<<"------LOOP GAME!!------"<<endl;
 
-    my_game->startGame(3000);
+    my_game->startGame(5500000);
     string nameFile="buffer_"+conf.idNumber+".csv";
     toCsvString(conf.home+"/car_model/exp/buffer/"+nameFile, my_game->buffer);
 
