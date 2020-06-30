24ce3cd60d08649a8dada1855c13481f8c70fd32
eran <eran505@gmail.com>
2020-07-01 00:33:10 +0300

    07/01 00:33:10

--- bash/run.sh
+++ bash/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-PATHtoFileDir=${HOME}"/eran/repo/track_racing/cmake-build-debug/"
+PATHtoFileDir=${HOME}"/eran/repo/track_racing/"
 
 for i in {0..100}
 do
   echo "-->$i<--"
   cd "${PATHtoFileDir}" || exit
-  ./track_racing
+  ./track_racing 2
 done
